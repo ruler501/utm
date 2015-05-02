859e0341230398060417440e9053a14a1b9bba0d
Devon Richards <d.ruler501@gmail.com>
2015-05-02 17:35:46 -0500

    Numerous changes and fixes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ on each line you can do one of 4 things, you can loop, increment a variable, dec
 	while i decr {
 		...
 	}
-Means while i is not all zero's subtract one then do the block. For increment it is while i is not all ones. Both flip the bits when done(incr end with all 0's decr all 1's)
+Means while i does not overflow subtract one then do the block. For increment it is while i is not all ones. Both flip the bits when done(incr end with all 0's decr all 1's). Can only be nested two deep.
 
 ###Increment:
 	incr(i) 
@@ -22,19 +22,22 @@ subtracts 1 from i
 
 ###Assignment:
 	a = i
-creates a new variable a equal to i, you cannot assign to an existing variable.
+creates a new variable a equal to i, you cannot assign to an existing variable you can assign to an existing variable, just don't overrun it's length, that causes undefined behavior
 
 ###Assignment with Allocation
 	a = i,5
-creates a new variable a equal to i, with 5 extra bits.
+creates a new variable a equal to i, with 5 extra bits. If you assign a variable to itself it is zeroed out.
 
 ###Assignment of a Constant
     a = 35,6
-Creates a new variable a equal to 35%(2**6), in 6 bits
+Creates a new variable a equal to 35%(2**6), in 6 bits,
 
 ##Syntax of the Output
     [current_state],[current_symbol]
     [new_state],[new_symbol], [> or < or -]
 
 ##Example
-For a working example see UTM.utm in this repository
+Multiplication.utm multiplies i(the original variable on the tape) by 5 and stores it in b
+
+Subtraction.utm subtracts 5 from i
+
--- UTM.py
+++ UTM.py
@@ -40,6 +40,7 @@ def moveV(pos,nextstate,movedirection,i):
         outStr += "start"+str(i)+",1\n"
         outStr += "start"+str(i)+",1,>\n\n"
     elif pos < curPos:
+        curPos += 1
         if curPos-pos > 1:
             outStr += "moveleft0mv"+str(i)+",_\n"
             outStr += nextstate+",_,"+movedirection+'\n\n'
@@ -66,18 +67,188 @@ def moveV(pos,nextstate,movedirection,i):
         outStr += "start"+str(i)+",1,<\n\n"
     curPos = pos
     return outStr
+
+def moveHeadV(pos,nextstate,movedirection,i):
+    global curPos
+    outStr = ""
+    if pos > 0:
+        if pos> 1:
+            outStr += "moveright0mv"+str(i)+",_\n"
+            outStr += nextstate+",_,"+movedirection+'\n\n'
+            outStr += "moveright0mv"+str(i)+",0\n"
+            outStr += nextstate+",0,"+movedirection+'\n\n'
+            outStr += "moveright0mv"+str(i)+",1\n"
+            outStr += nextstate+",1,"+movedirection+'\n\n'
+        for j in range(1,pos):
+            outStr += "moveright"+str(j)+"mv"+str(i)+",_\n"
+            outStr += "moveright"+str(j-1)+"mv"+str(i)+',_,>\n\n'
+            outStr += "moveright"+str(j)+"mv"+str(i)+",0\n"
+            outStr += "moveright"+str(j-1)+"mv"+str(i)+',0,>\n\n'
+            outStr += "moveright"+str(j)+"mv"+str(i)+",1\n"
+            outStr += "moveright"+str(j-1)+"mv"+str(i)+',1,>\n\n'
+        outStr += "start"+str(i)+",_\n"
+        if pos > 1:
+            outStr += "moveright"+str(pos-2)+"mv"+str(i)+",_,>\n\n"
+        else:
+            outStr += nextstate+",_,"+movedirection+'\n\n'
+        outStr += "start"+str(i)+",0\n"
+        if pos > 1:
+            outStr += "moveright"+str(pos-2)+"mv"+str(i)+",0,>\n\n"
+        else:
+            outStr += nextstate+",0,"+movedirection+'\n\n'
+        outStr += "start"+str(i)+",1\n"
+        if pos > 1:
+            outStr += "moveright"+str(pos-2)+"mv"+str(i)+",1,>\n\n"
+        else:
+            outStr += nextstate+",,"+movedirection+'\n\n'
+    elif pos < 0:
+        curPos += 1
+        if -pos > 1:
+            outStr += "moveleft0mv"+str(i)+",_\n"
+            outStr += nextstate+",_,"+movedirection+'\n\n'
+            outStr += "moveleft0mv"+str(i)+",0\n"
+            outStr += nextstate+",0,"+movedirection+'\n\n'
+            outStr += "moveleft0mv"+str(i)+",1\n"
+            outStr += nextstate+",1,"+movedirection+'\n\n'
+        for j in range(1,-pos):
+            outStr += "moveleft"+str(j)+"mv"+str(i)+",_\n"
+            outStr += "moveleft"+str(j-1)+"mv"+str(i)+',_,<\n\n'
+            outStr += "moveleft"+str(j)+"mv"+str(i)+",0\n"
+            outStr += "moveleft"+str(j-1)+"mv"+str(i)+',0,<\n\n'
+            outStr += "moveleft"+str(j)+"mv"+str(i)+",1\n"
+            outStr += "moveleft"+str(j-1)+"mv"+str(i)+',1,<\n\n'
+        outStr += "start"+str(i)+",_\n"
+        if -pos > 1:
+            outStr += "moveleft"+str(-pos-2)+"mv"+str(i)+",_,<\n\n"
+        else:
+            outStr += nextstate+",_,"+movedirection+'\n\n'
+        outStr += "start"+str(i)+",0\n"
+        if -pos > 1:
+            outStr += "moveleft"+str(-pos-2)+"mv"+str(i)+",0,<\n\n"
+        else:
+            outStr += nextstate+",0,"+movedirection+'\n\n'
+        outStr += "start"+str(i)+",1\n"
+        if -pos > 1:
+            outStr += "moveleft"+str(-pos-2)+"mv"+str(i)+",1,<\n\n"
+        else:
+            outStr += nextstate+",1,"+movedirection+'\n\n'
+    else:
+        outStr += "start"+str(i)+",_\n"
+        outStr += nextstate+",_,"+movedirection+"\n\n"
+        outStr += "start"+str(i)+",0\n"
+        outStr += nextstate+",0,"+movedirection+"\n\n"
+        outStr += "start"+str(i)+",1\n"
+        outStr += nextstate+",1,"+movedirection+"\n\n"
+    curPos = pos
+    return outStr
+
+def copyOverV(opos, newpos, nextstate, i):
+    global curPos
+    outStr = ""
+    if newpos <= curPos:
+        outStr = moveV(newpos,"startcve"+str(i),">",i)
+        curPos = newpos
+    else:
+        outStr = moveV(newpos,"startcve"+str(i),">",i)
+        curPos = newpos
+    if opos <= curPos:
+        outStr += moveV(opos,"startcv"+str(i),">","r"+str(i))
+    else:
+        outStr += moveV(opos,"startcv"+str(i),">","r"+str(i))
+    curPos = newpos
+    outStr += moveV(opos,"startcvc0"+str(i),"-","c0m"+str(i))
+    curPos = newpos
+    outStr += moveV(opos,"startcvc1"+str(i),"-","c1m"+str(i))
+    
+    outStr += "startcve"+str(i)+",0\n"
+    if opos <= curPos:
+        outStr += "startr"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startr"+str(i)+",_,>\n\n"
+    outStr += "startcve"+str(i)+",1\n"
+    if opos <= curPos:
+        outStr += "startr"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startr"+str(i)+",_,>\n\n"
+    outStr += "startcvec1"+str(i)+",_\n"
+    if opos <= curPos:
+        outStr += "startc1m"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startc1m"+str(i)+",_,>\n\n"
+    outStr += "startcvec1"+str(i)+",0\n"
+    if opos <= curPos:
+        outStr += "startc1m"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startc1m"+str(i)+",_,>\n\n"
+    outStr += "startcvec1"+str(i)+",1\n"
+    if opos <= curPos:
+        outStr += "startc1m"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startc1m"+str(i)+",_,>\n\n"
+    outStr += "startcvec0"+str(i)+",_\n"
+    if opos <= curPos:
+        outStr += "startc1m"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startc1m"+str(i)+",_,>\n\n"
+    outStr += "startcvec0"+str(i)+",0\n"
+    if opos <= curPos:
+        outStr += "startc0m"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startc0m"+str(i)+",_,>\n\n"
+    outStr += "startcvec0"+str(i)+",1\n"
+    if opos <= curPos:
+        outStr += "startc0m"+str(i)+",_,<\n\n"
+    else:
+        outStr += "startc0m"+str(i)+",_,>\n\n"
+    outStr += "startcv"+str(i)+",_\n"
+    outStr += nextstate+",_,>\n\n"
+    outStr += "startcv"+str(i)+",0\n"
+    outStr += "copyright"+str(newpos-opos)+"cv"+str(i)+"c0,_,>\n\n"
+    outStr += "startcv"+str(i)+",1\n"
+    outStr += "copyright"+str(newpos-opos)+"cv"+str(i)+"c1,_,>\n\n"
+    outStr += "startcvc0"+str(i)+",_\n"
+    outStr += "startcv"+str(i)+",0,>\n\n"
+    outStr += "startcvc1"+str(i)+",_\n"
+    outStr += "startcv"+str(i)+",1,>\n\n"
+    outStr += "copyright0cv"+str(i)+"c1,_\n"
+    outStr += "startcvec1"+str(i)+",1,>\n\n"
+    outStr += "copyright0cv"+str(i)+"c1,0\n"
+    outStr += "copyright0cv"+str(i)+"c1,0,>\n\n"
+    outStr += "copyright0cv"+str(i)+"c1,1\n"
+    outStr += "copyright0cv"+str(i)+"c1,1,>\n\n"
+    outStr += "copyright0cv"+str(i)+"c0,_\n"
+    outStr += "startcvec0"+str(i)+",0,>\n\n"
+    outStr += "copyright0cv"+str(i)+"c0,0\n"
+    outStr += "copyright0cv"+str(i)+"c0,0,>\n\n"
+    outStr += "copyright0cv"+str(i)+"c0,1\n"
+    outStr += "copyright0cv"+str(i)+"c0,1,>\n\n"
+    for j in range(1,newpos-opos+1):
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c1,_\n"
+        outStr += "copyright"+str(j-1)+"cv"+str(i)+"c1,_,>\n\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c1,0\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c1,0,>\n\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c1,1\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c1,1,>\n\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c0,_\n"
+        outStr += "copyright"+str(j-1)+"cv"+str(i)+"c0,_,>\n\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c0,0\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c0,0,>\n\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c0,1\n"
+        outStr += "copyright"+str(j)+"cv"+str(i)+"c0,1,>\n\n"
+    curPos = opos+1
+    return outStr
     
 def copyV(opos, newpos, nextstate, i):
     global curPos
     outStr = ""
     if opos <= curPos:
-        outStr = moveV(opos-1,"startcv"+str(i),">",i)
+        outStr = moveV(opos,"startcv"+str(i),">",i)
     elif opos > curPos:
-        outStr = moveV(opos,"startcv"+str(i),"<",i)
+        outStr = moveV(opos,"startcv"+str(i),">",i)
     curPos = newpos+1
-    outStr += moveV(opos,"startcvc0"+str(i),"-","c0m"+str(i))
+    outStr += moveV(opos+1,"startcvc0"+str(i),"-","c0m"+str(i))
     curPos = newpos+1
-    outStr += moveV(opos,"startcvc1"+str(i),"-","c1m"+str(i))
+    outStr += moveV(opos+1,"startcvc1"+str(i),"-","c1m"+str(i))
 
     outStr += "startcv"+str(i)+",_\n"
     outStr += nextstate+",_,>\n\n"
@@ -120,20 +291,63 @@ def copyV(opos, newpos, nextstate, i):
 def copyVresize(opos, newpos, resize, nextstate, i):
     global curPos
     outStr = moveV(newpos, "startcvr"+str(i), ">", i)
+    outStr += "startcvr"+str(i)+",0\n"
+    if resize > 1:
+        outStr += "out"+str(resize-2)+"c0"+str(i)+",0,>\n\n"
+    else:
+        outStr += "startc"+str(i)+",0,-\n\n"
+    outStr += "startcvr"+str(i)+",1\n"
+    if resize > 1:
+        outStr += "out"+str(resize-2)+"c0"+str(i)+",0,>\n\n"
+    else:
+        outStr += "startc"+str(i)+",0,-\n\n"
     outStr += "startcvr"+str(i)+",_\n"
     if resize > 1:
         outStr += "out"+str(resize-2)+"c0"+str(i)+",0,>\n\n"
         outStr += "out0c0"+str(i)+",_\n"
+        outStr += "startc"+str(i)+",0,-\n\n"
+        outStr += "out0c0"+str(i)+",0\n"
+        outStr += "startc"+str(i)+",0,-\n\n"
+        outStr += "out0c0"+str(i)+",1\n"
         outStr += "startc"+str(i)+",0,-\n\n"
     else:
         outStr += "startc"+str(i)+",0,-\n\n"
     for j in range(1,resize):
         outStr += "out"+str(j)+"c0"+str(i)+",_\n"
         outStr += "out"+str(j-1)+"c0"+str(i)+",0,>\n\n"
+        outStr += "out"+str(j)+"c0"+str(i)+",0\n"
+        outStr += "out"+str(j-1)+"c0"+str(i)+",0,>\n\n"
+        outStr += "out"+str(j)+"c0"+str(i)+",1\n"
+        outStr += "out"+str(j-1)+"c0"+str(i)+",0,>\n\n"
     curPos = newpos
-    outStr += copyV(opos,newpos,nextstate,"c"+str(i))
+    if newpos in variables.values():
+        outStr += copyOverV(opos,newpos,nextstate,"c"+str(i))
+    else:
+        outStr += copyV(opos,newpos,nextstate,"c"+str(i))
     return outStr
-        
+
+def copyVself(opos, resize, nextstate, i):
+    global curPos
+    outStr = moveV(opos,"startcvs"+str(i),">",i)
+    outStr += "startcvs"+str(i)+",0\n"
+    outStr += "startcvs"+str(i)+",0,>\n\n"
+    outStr += "startcvs"+str(i)+",1\n"
+    outStr += "startcvs"+str(i)+",0,>\n\n"
+    outStr += "startcvs"+str(i)+",_\n"
+    if resize > 1:
+        outStr += "fillzeroes"+str(resize-2)+str(i)+",0,>\n\n"
+        outStr += "fillzeroes0"+str(i)+",_\n"
+        outStr += nextstate+",0,-\n\n"
+        for j in range(1,resize-1):
+            outStr += 'fillzeroes'+str(j)+str(i)+",_\n"
+            outStr += 'fillzeroes'+str(j-1)+str(i)+",0,>\n\n"
+    else:
+        if resize < 1:
+            outStr += nextstate+",_,<\n\n"
+        else:
+            outStr += nextstate+",0,-\n\n"
+    return outStr
+
 def outputConstant(pos,nextstate,constant,i,width):
     global curPos
     outStr = moveV(pos,"starto"+str(i),">",i)
@@ -143,11 +357,23 @@ def outputConstant(pos,nextstate,constant,i,width):
         outStr += "output1c"+b[1]+str(i)+","+b[0]+",>\n\n"
     else:
         outStr += nextstate+","+b[0]+",-\n\n"
+    outStr += "starto"+str(i)+",0\n"
+    outStr += "output1c"+b[1]+str(i)+","+b[0]+",>\n\n"
+    outStr += "starto"+str(i)+",1\n"
+    outStr += "output1c"+b[1]+str(i)+","+b[0]+",>\n\n"
     for j in range(1,len(b)-1):
         outStr += "output"+str(j)+"c"+b[j]+str(i)+",_\n"
         outStr += "output"+str(j+1)+"c"+b[j+1]+str(i)+","+b[j]+",>\n\n"
+        outStr += "output"+str(j)+"c"+b[j]+str(i)+",0\n"
+        outStr += "output"+str(j+1)+"c"+b[j+1]+str(i)+","+b[j]+",>\n\n"
+        outStr += "output"+str(j)+"c"+b[j]+str(i)+",1\n"
+        outStr += "output"+str(j+1)+"c"+b[j+1]+str(i)+","+b[j]+",>\n\n"
     if len(b) > 1:
         outStr += "output"+str(len(b)-1)+"c"+b[-1]+str(i)+",_\n"
+        outStr += nextstate+","+b[-1]+",-\n\n"
+        outStr += "output"+str(len(b)-1)+"c"+b[len(b)-1]+str(i)+",0\n"
+        outStr += nextstate+","+b[-1]+",-\n\n"
+        outStr += "output"+str(len(b)-1)+"c"+b[len(b)-1]+str(i)+",1\n"
         outStr += nextstate+","+b[-1]+",-\n\n"
     curPos = pos
     return outStr
@@ -180,7 +406,7 @@ def decrement(pos,nextstate,errorstate,i):
 inFile = open("UTM.utm")
 out = open("UTM.utmo","w")
 functions = {'incr': increment, 'decr': decrement}
-inLines=inFile.readlines()
+inLines=inFile.readlines()#[:-1]
 
 out.write("name: Auto-Generated\ninit: start0\naccept: end\n\n")
 
@@ -190,6 +416,9 @@ for lineno in range(len(inLines)):
     line = inLines[lineno].strip()
     if line == "}":
         i += 1
+        for loop in whileLoops:
+            if lineno == loop[2]+1:
+                curPos=loop[3]
         continue
     if line[:5] == "while":
         pieces = line.split()
@@ -198,14 +427,25 @@ for lineno in range(len(inLines)):
         vposition = variables[pieces[1]]    
         function = pieces[2]
         counter = pieces[3][:-1]
+        count = 1
+        nested = False
         for endno in range(lineno+1,len(inLines)):
+            if '{' in inLines[endno]:
+                count += 1
             if inLines[endno].strip() == "}":
-                whileLoops.append((len(whileLoops), lineno, endno-1, counter))
-                if endno == len(inLines) - 1:
-                    out.write(functions[function](vposition, "start"+str(i+1), "end",i))
-                else:
-                    out.write(functions[function](vposition, "start"+str(i+1), "start"+str(endno+1),i))
-                break
+                count -= 1
+                if count == 0:
+                    for loop in whileLoops:
+                        if loop[1] < i and loop[2] > i:
+                            nested = True
+                    whileLoops.append((len(whileLoops), lineno, endno-1, vposition, function, nested))                    
+                    if nested:
+                        if endno == len(inLines) - 1:
+                            out.write(functions[function](vposition, "start"+str(i+1), "end",i))
+                        else:
+                            out.write(functions[function](vposition, "start"+str(i+1), "start"+str(endno+1),i))
+                    curPos=vposition
+                    break
         else:
             raise SyntaxError("While loop never finished")
         i+=1
@@ -213,24 +453,34 @@ for lineno in range(len(inLines)):
     if "=" in line:
         pieces = line.split("=")
         if pieces[0] in variables:
-            raise NameError("Can't copy over an existing variable, "+pieces[0]+" already exists")
+            pass
+            #raise NameError("Can't copy over an existing variable, "+pieces[0]+" already exists")
         variable = pieces[1]
         if variable not in variables:
             if "," in variable:
                 try:
                     allocatedSpace = int(variable.split(',')[1])
                     if variable.split(',')[0] in variables:
-                        variables[pieces[0]] = max(variables.items(), key=operator.itemgetter(1))[1]+1
-                        if i == len(inLines) - 1:
-                            out.write(copyVresize(variables[variable.split(',')[0]],variables[pieces[0]],allocatedSpace,"end",i))
+                        if pieces[0] not in variables:
+                            variables[pieces[0]] = max(variables.items(), key=operator.itemgetter(1))[1]+1
+                        if variable.split(',')[0] == pieces[0]:
+                            if i == len(inLines) - 1:
+                                out.write(copyVself(variables[pieces[0]],variables[variable.split(',')[0]],allocatedSpace,"end",i))
+                            else:
+                                print(variables[pieces[0]])
+                                out.write(copyVself(variables[pieces[0]],allocatedSpace,"start"+str(i+1),i))
                         else:
-                            out.write(copyVresize(variables[variable.split(',')[0]],variables[pieces[0]],allocatedSpace,"start"+str(i+1),i))
+                            if i == len(inLines) - 1:
+                                out.write(copyVresize(variables[variable.split(',')[0]],variables[pieces[0]],allocatedSpace,"end",i))
+                            else:
+                                out.write(copyVresize(variables[variable.split(',')[0]],variables[pieces[0]],allocatedSpace,"start"+str(i+1),i))
                         i+=1
                         continue
                     else:
                         constant = int(variable.split(',')[0])
-                        variables[pieces[0]] = max(variables.items(), key=operator.itemgetter(1))[1]+1
-                        if i == len(inLines) - 1:
+                        if pieces[0] not in variables:
+                            variables[pieces[0]] = max(variables.items(), key=operator.itemgetter(1))[1]+1
+                        if lineno == len(inLines) - 1:
                             out.write(outputConstant(variables[pieces[0]],"end",constant,i,allocatedSpace))
                         else:
                             out.write(outputConstant(variables[pieces[0]],"start"+str(i+1),constant,i,allocatedSpace))
@@ -241,11 +491,17 @@ for lineno in range(len(inLines)):
                     raise NameError(variable+" is not a defined variable")
             else:
                 raise NameError(variable+" is not a defined variable")
-        variables[pieces[0]] = max(variables.items(), key=operator.itemgetter(1))[1]+1
-        if i == len(inLines) - 1:
-            out.write(copyV(variables[variable],variables[pieces[0]],"end",i))
+        if pieces[0] not in variables:
+            variables[pieces[0]] = max(variables.items(), key=operator.itemgetter(1))[1]+1
+            if lineno == len(inLines) - 1:
+                out.write(copyV(variables[variable],variables[pieces[0]],"end",i))
+            else:
+                out.write(copyV(variables[variable],variables[pieces[0]],"start"+str(i+1),i))
         else:
-            out.write(copyV(variables[variable],variables[pieces[0]],"start"+str(i+1),i))
+            if lineno == len(inLines) - 1:
+                out.write(copyOverV(variables[variable],variables[pieces[0]],"end",i))
+            else:
+                out.write(copyOverV(variables[variable],variables[pieces[0]],"start"+str(i+1),i))
         i += 1
         continue
     function = line.split('(')[0]
@@ -258,9 +514,18 @@ for lineno in range(len(inLines)):
         else:
             raise NameError(variable+" is not a defined variable")
     for loop in whileLoops:
-        if i == loop[2]:
-            out.write(functions[function](variables[variable], "start"+str(loop[1]), "start"+str(loop[1]), i))
-            break
+        if lineno == loop[2]:
+            if loop[5]:
+                out.write(functions[function](variables[variable], "startn"+str(loop[1]), "start"+str(loop[1]), i))
+                out.write(functions[loop[4]](loop[3], "start"+str(loop[1]+1), "start"+str(loop[2]+2),"n"+str(loop[1])))
+                break
+            else:
+                out.write(functions[function](variables[variable], "start"+str(loop[1]), "start"+str(loop[1]), i))
+                if endno == len(inLines) - 1:
+                    out.write(functions[loop[4]](loop[3], "start"+str(loop[1]+1), "end",loop[1]))
+                else:
+                    out.write(functions[loop[4]](loop[3], "start"+str(loop[1]+1), "start"+str(loop[2]+2),loop[1]))
+                break
     else:
         if lineno == len(inLines)-1:
             out.write(functions[function](variables[variable], "end", "end", i))
